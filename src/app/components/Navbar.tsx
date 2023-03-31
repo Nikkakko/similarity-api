5d0533b4045164d8ec02c250638fc70735c57074
Nika Kopadze <nikolozkopadze@gmail.com>
2023-03-31 21:52:42 +0400

    updated button and layout

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,24 +2,20 @@ import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { FC } from 'react';
 import { buttonVariants } from './ui/Button';
-import SignInButton from './SignInButton.tsx';
 import SignOutButton from './SignOutButton';
+import SignInButton from './SignInButton.tsx';
 
 const Navbar = async ({}) => {
   const session = await getServerSession();
 
   return (
-    <div
-      className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900
-    z-50 top:0 left:0 right-0 h-20 border-b border-slate-300 darkL:border-slate-700 
-    shadow-md flex items-center justify-between'
-    >
+    <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
       <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
         <Link href='/' className={buttonVariants({ variant: 'link' })}>
-          Text Similarity 1.0
+          Text Similarity v1.0
         </Link>
 
-        <div className='md:hidden'>{/* <TemeToggle /> */}</div>
+        <div className='md:hidden'>{/* <ThemeToggle /> */}</div>
 
         <div className='hidden md:flex gap-4'>
           {/* <ThemeToggle /> */}
@@ -29,12 +25,11 @@ const Navbar = async ({}) => {
           >
             Documentation
           </Link>
-
           {session ? (
             <>
               <Link
-                href='/dashboard'
                 className={buttonVariants({ variant: 'ghost' })}
+                href='/dashboard'
               >
                 Dashboard
               </Link>
