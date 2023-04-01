d7ca230be35fc749275482f68c50585fbbd6b003
Nika Kopadze <nikolozkopadze@gmail.com>
2023-04-01 20:49:38 +0400

    fixed

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,25 @@
 import { FC } from 'react';
 
-interface pageProps {}
+import type { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+import { db } from '@/lib/db';
+import { authOptions } from '@/lib/auth';
 
-const page: FC<pageProps> = ({}) => {
+export const metadata: Metadata = {
+  title: 'Similarity API | Dashboard',
+  description: 'Free & Open Source Similarity API',
+};
+
+const page = async ({}) => {
+  const user = getServerSession(authOptions);
+
+  if (!user) return notFound();
+
+  const apiKey = await db.apiKey.findFirst({
+    where: { userId: user.user.id },
+  });
+
   return <div>page</div>;
 };
 
