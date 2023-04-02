36b78d27829fd7eaa19f85ee83b26ec0b7bdaf06
Nika Kopadze <nikolozkopadze@gmail.com>
2023-04-02 12:08:06 +0400

    added table

--- src/app/components/ApiDashboard.tsx
+++ src/app/components/ApiDashboard.tsx
@@ -7,6 +7,7 @@ import { formatDistance } from 'date-fns';
 import LargeHeading from './ui/LargeHeading';
 import Paragraph from './ui/Paragraph';
 import { Input } from './ui/Input';
+import Table from './Table';
 
 const ApiDashboard = async ({}) => {
   const user = await getServerSession(authOptions);
@@ -40,7 +41,13 @@ const ApiDashboard = async ({}) => {
       <div className='flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center'>
         <Paragraph>Your API key:</Paragraph>
         <Input className='w-fit truncate' readOnly value={activeApiKey.key} />
+        {/* Add options to create new /revoke */}
       </div>
+
+      <Paragraph className='text-center md:text-left mt-4 mb-4'>
+        Your Api History:
+      </Paragraph>
+      <Table userRequests={serializableRequests} />
     </div>
   );
 };
